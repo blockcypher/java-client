fb69fc96414bb6e87ede8a30fa606d5f8f999732
Josh Cincinnati <acityinohio@users.noreply.github.com>
2015-09-08 23:30:54 +0000

    remove path and consume packages from nulldata

--- src/main/java/com/blockcypher/model/nulldata/NullData.java
+++ src/main/java/com/blockcypher/model/nulldata/NullData.java
@@ -3,10 +3,6 @@ package com.blockcypher.model.nulldata;
 import com.blockcypher.utils.gson.GsonFactory;
 import org.apache.log4j.Logger;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.Path;
-import javax.ws.rs.core.MediaType;
-
 /**
  * Representation of a NullData, ie:
  *{
@@ -15,8 +11,6 @@ import javax.ws.rs.core.MediaType;
  *  "hash": "cb6974e0fd57c91b70403e85ef48c840eecdca4804dfc4897b1321d5328e4f18"
  *}
  */
-@Path("/txs/data/")
-@Consumes(MediaType.APPLICATION_JSON)
 public class NullData {
 
     private static final Logger logger = Logger.getLogger(NullData.class);
